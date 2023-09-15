9249afa9d948536d4b549e83f3cd0b55172186cf
dh-teja2047 <[dh.teja2047@gmail.com]>
2023-09-15 22:26:25 +0530

    Project4 repository created

---
+++ script.js
@@ -0,0 +1,62 @@
+
+const display = document.getElementById("display");
+const displayInput = " ";
+
+function getInput(input) {
+  //const input = input;
+  display.innerHTML = input;
+  //  displayInput += input;
+
+  }
+/*
+const value = document.getElementById("keys");
+const displayInput = "";
+function getInput(input) {
+keys.innerHTML = input;
+displayInput += input;
+}
+
+function deleteDigit() {
+  display.innerHTML;
+}
+
+function calculate() {
+  let digit1;
+  let digit2;
+  let result = 0;
+  let symbol;
+
+}
+
+/*
+function calculate() {
+  let digit;
+  let symbol;
+  let result = 0;
+
+  for (var i = 0; i < displayInput.length; i++) {
+    if (displayInput[i] == '+') {
+
+    }
+    if (displayInput[i] == '-'){
+
+    }
+    if (displayInput[i] == '*'){
+
+    }
+    if (displayInput[i] == '/'){
+
+    }
+    else{
+      digit = displayInput[i];
+    }
+  }
+
+}
+*/
+
+
+//display.innerHTML = finalresult;
+//pop()
+//push()
+//splice()
